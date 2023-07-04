32086661a5f3c8d7b651f68a84e51e82b62f1006
PoliJun <rivenloving@gmail.com>
2023-07-04 20:59:30 +0800

    Lecture 9 1:44

--- hello/app.py
+++ hello/app.py
@@ -3,12 +3,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
+    return render_template("index.html")
+
+
+# @app.route("/index.html")
+# def index():
     # If there is no if else blocks, there would be a no key error.
     # So, request.args is a dictionary.
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-        name = "world"
-    return render_template('index.html')
+    # if "name" in request.args:
+    #     name = request.args["name"]
+    # else:
+    #     name = "world"
+
+    # or just use
+    # This wouldn't throw an error, just return a  none.Default value is "world".
+    # name = request.args.get("name", "world")
+#     return render_template('index.html')
+
+# @app.route("/greet", methods=["POST"])
+# def greet():
+#     return render_template('greet.html', name=request.args.get("name", "world"))
--- week-9.md
+++ week-9.md
@@ -2,8 +2,23 @@
 
 ## Flask
 - app.py
-
 - requirements.txt
 - static/
 - templates/
-    - double curly braces `{{name}}`
+    - ***Jinja***: double curly braces `{{name}}`
+    - not only Jinja, but also whole html: jango-html
+    - extends the main layout, just modify the smaller fragments.
+- POST and GET
+    - POST is send info to the server, just like adding goods to your shopping cart.
+    - GET is get info from the server
+    - `request.args` is only used for `GET`
+    - use `request.form` for `POST`
+- Jinja allows you to loop  `<li>` , dictionary:
+    > `{% for name in registrants %}{% endfor %}`
+- **It is capable to attack the remote server through local browser, if server not veryfied the message that the client's browser send!**
+
+## input types
+- text
+- radio
+- hidden
+    > Borowsers can send information to server. So, it is not save even the type is *hidden*. We should authoritize special actions on server side.
